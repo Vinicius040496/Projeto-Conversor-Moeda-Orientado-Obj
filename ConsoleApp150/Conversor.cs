d32dd93b6cb7e8352ab1f0e330e8e009ecffa9fe
Vinicius040496 <vinicius.almeida2011@gmail.com>
2024-06-15 18:44:24 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp150/Conversor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp150
+{
+    internal class Conversor
+    {
+        public double ValorDolar {  get; set; }
+        public double Quantidade {  get; set; }
+
+        public double Imposto()
+        {
+            return ValorDolar * Quantidade * 0.06;
+        }
+        public double ValorFinal()
+        {
+            return Imposto() + (ValorDolar * Quantidade);
+        }
+        public override string ToString()
+        {
+            return "Valor a ser pago em reais na compra com 6% de iof: " + ValorFinal().ToString("f2");
+        }
+    }
+}
---
+++ ConsoleApp150/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp150
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conversor Calc = new Conversor();
+
+            Console.WriteLine("digite valor do dolar hoje: ");
+            Calc.ValorDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantos dolares gostaria de comprar: ");
+            Calc.Quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Calc.Imposto();
+            Calc.ValorFinal();
+            Console.WriteLine(Calc);
+            Console.ReadLine();
+        }
+    }
+}
